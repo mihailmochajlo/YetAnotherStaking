93fbcd919fea4524717de12d5d5429e0f9da1216
Mykhailo Mochailo <mihail.mochajlo@gmail.com>
2023-05-05 13:05:53 +0100

    added env lib and updated configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  networks: {
+    bsctestnet: {
+      url: "https://data-seed-prebsc-1-s3.binance.org:8545",
+      chainId: 97,
+      accounts: {
+        mnemonic:  process.env.MNEMONIC
+      }
+    }
+  }
 };
